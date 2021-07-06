530ba6c085a847b9915d2fc59a46154554c9898a
ureshii-ww <ggggxxxx359@gmail.com>
2021-07-06 19:16:06 +0700

    make right regex for replaceClasses

--- src/scripts/replaceClassesInCss.js
+++ src/scripts/replaceClassesInCss.js
@@ -1,7 +1,7 @@
 export const replaceClassesInCss = (css, classMap) => {
   for (const key in classMap) {
     if (css.includes(key)) {
-      const regex = new RegExp(key, 'g')
+      const regex = new RegExp(`${key}\\b(?![\\w-])`, 'g')
       css = css.replace(regex, classMap[key]);
     }
   }
