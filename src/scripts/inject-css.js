144d6370c00c83c3aeac7c4d4824d68c2ccac85f
ureshii-ww <ggggxxxx359@gmail.com>
2021-07-06 21:10:05 +0700

    fix code

--- src/scripts/inject-css.js
+++ src/scripts/inject-css.js
@@ -1,8 +1,7 @@
 export const injectCss = (css) => {
-  const body = document.getElementsByTagName('body')[0];
   const newCss = document.createElement('style');
   newCss.type = "text/css";
   newCss.id = 'DTF Nocturne';
   newCss.innerHTML = css;
-  body.appendChild(newCss);
+  document.body.appendChild(newCss);
 }
